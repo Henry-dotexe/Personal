0cffec0708c5a13427d526e6d082de330055e381
Henry Gabriel <hsotolima@frba.utn.edu.ar>
2021-09-01 20:11:34 -0300

    SimulacroLogico terminado

--- SimulacroLogico.pl
+++ SimulacroLogico.pl
@@ -1,3 +1,4 @@
+%Henry Soto - Paradigmas de programación 
 % contenido( Titulo, Contenido)
 %    peli(Protagonistas, Reparto)
 %    serie(Temporada, Protagonistas, Reparto) 
@@ -6,6 +7,16 @@ contenido(elPadrino, peli( [ alpaccino, brando ], [] )).
 contenido(avengers, peli([ downeyjr, evans, ruffalo, hemsworth, johansson, pratt ], [ samuelJackson, dinklage ])).
 contenido(friends, serie(1, [ cox, anniston ], [ typer ])).
 contenido(friends, serie(8, [ cox, anniston ], [ pitt ])).
+
+contenido(got,serie(1,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(2,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(3,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(4,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(5,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(6,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(7,[clarke,kit,dinklage],[momoa,gillen])).
+contenido(got,serie(8,[clarke,kit,dinklage],[momoa,gillen])).
+
 % personaje(Nombre, Fans, Skills)
 personaje(alpaccino,4000,[violento]).
 personaje(brando, 4000, [violento, pokerFace]).
@@ -14,11 +25,14 @@ personaje(francella, 200, [comico, serio]).
 
 personaje(downeyjr,10000,[filantropo,millonario,playboy]).
 personaje(samuelJackson,5000,[serio]).
+
 %mauskerramienta
 esActor(Alguien) :- participaEnContenido(Alguien,_).
 esContenido(Contenido) :- contenido(Contenido,_).
+
 %1. popular/1. Un personaje es popular si tiene mas de 3mil fans
 popular(Alguien) :- personaje(Alguien,Fans,_),Fans>3000.
+
 %6.b Alguien es popular tambien si admite multiverso
 popular(Alguien) :- personaje(Alguien,_,_),admiteMultiverso(Alguien).
 
@@ -60,11 +74,26 @@ sonInseparables(Actor1,Actor2) :- forall(participaEnContenido(Actor1,Titulo),par
 %9. ¿Qué predicados debería modificar para agregar un nuevo tipo de contenido?
 % ¿Qué concepto me ayuda a realizarlo de forma simple?
 
-%participaEnContenido,esProtagonista son los unicos predicados que se modifiquen
-%El concepto...
+%participaEnContenido,esProtagonista son los unicos predicados que se modificarían 
+%El concepto que permite esta simplicidad es el polimorfismo, ya que ambos predicados funcionan con todos los tipos de contenido definidos,
+%por lo que el resto de predicados que los utilizan pueden tratar los datos como iguales
 
-%10.Polimorfismo...
-%Patter matching se utiliza en los predicados participaEnContenido y esProtagonista para matchear entre pelis y series
-%Orden superior se utiliza en todos los predicados que usan findall o forall (Puntos 3,4,5,6,7,8)
+%10.Indique al menos una vez donde se haga uso de los siguientes conceptos
+%Polimorfismo: En los predicados participaEnContenido y esProtagonista, ya que contemplan todos los tipos de contenido
+%Patter matching: También se utiliza en los predicados participaEnContenido y esProtagonista para matchear entre pelis y series
+%Orden superior: Se utiliza en todos los predicados que usan findall o forall (Puntos 3,4,5,6,7,8)
 
-%11
+%11. Se pide saber si se puede resumir una serie de una temporada a otra
+%Una serie se puede resumir si existen todas las temporadas en el rango pedido
+
+temporadaCargada(Titulo,Temporada):-contenido(Titulo,serie(Temporada,_,_)).
+
+sePuedeResumir(Titulo,TemporadaInicio,TemporadaFinal) :- 
+    esSerie(Titulo), 
+    forall(between(TemporadaInicio,TemporadaFinal,Temporada),temporadaCargada(Titulo,Temporada)).
+
+%12. Se pide saber si una serie esta completa
+temporadasDeSerie(got,8). %Caso de prueba y para que zafe del error de predicado indefinido
+estaCompleta(Titulo) :- 
+    esSerie(Titulo),temporadasDeSerie(Titulo,Cantidad),
+    forall(between(1,Cantidad,Temporada),temporadaCargada(Titulo,Temporada)).
